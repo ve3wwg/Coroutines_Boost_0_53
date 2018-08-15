1cd141463933055b662119435c566e19f189c3fa
Warren Gay VE3WWG <ve3wwg@gmail.com>
2018-08-14 23:32:01 -0400

    rm commented code

--- epollcoro.cpp
+++ epollcoro.cpp
@@ -14,20 +14,6 @@
 
 #include "epollcoro.hpp"
 
-#if 0
-typedef union epoll_data {
-	void    *ptr;
-        int      fd;
-        uint32_t u32;
-        uint64_t u64;
-} epoll_data_t;
-
-struct epoll_event {
-	uint32_t     events;    /* Epoll events */
-        epoll_data_t data;      /* User data variable */
-};
-#endif           
-
 EpollCoro::EpollCoro() {
 	efd = epoll_create1(EPOLL_CLOEXEC);
 	assert(efd > 0);
