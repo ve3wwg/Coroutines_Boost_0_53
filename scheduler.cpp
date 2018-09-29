681ebe1e908ab0f4b462bdd12e93b660ff5f9e9d
Warren Gay VE3WWG <ve3wwg@gmail.com>
2018-09-29 10:25:30 -0400

    Scheduler class support

---
+++ scheduler.cpp
@@ -0,0 +1,103 @@
+//////////////////////////////////////////////////////////////////////
+// scheduler.cpp -- Scheduler Class Implementation
+// Date: Sat Aug 11 14:52:20 2018   (C) ve3wwg@gmail.com
+///////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/epoll.h>
+#include <assert.h>
+
+#include "scheduler.hpp"
+
+Scheduler::Scheduler() {
+	efd = epoll_create1(EPOLL_CLOEXEC);
+	assert(efd > 0);
+}
+
+Scheduler::~Scheduler() {
+	close(efd);
+}
+
+void
+Scheduler::close(int fd) {
+	int rc;
+
+	do	{
+		rc = ::close(fd);
+	} while ( rc == -1 && errno == EINTR );
+}
+
+bool
+Scheduler::del(int fd) {
+	int rc;
+
+	auto it = fdset.find(fd);
+	if ( it == fdset.end() ) {
+		errno = EBADF;
+		return false;
+	}
+
+	fdset.erase(it);
+	rc = epoll_ctl(efd,EPOLL_CTL_DEL,fd,nullptr);
+	return !rc;
+}
+
+bool
+Scheduler::add(int fd,uint32_t events,Service *co) {
+	struct epoll_event evt;
+	int rc;
+
+	evt.events = events;
+	evt.data.ptr = co;
+	rc = epoll_ctl(efd,EPOLL_CTL_ADD,fd,&evt);
+	return !rc;
+}
+
+bool
+Scheduler::chg(int fd,Events& ev,CoroutineBase *co) {
+	struct epoll_event evt;
+	int rc;
+
+	if ( ev.changes() == 0 )
+		return true;			// No changes
+
+	evt.events = ev.events();
+	evt.data.ptr = co;
+	rc = epoll_ctl(efd,EPOLL_CTL_MOD,fd,&evt);
+	return !rc;
+}
+
+void
+Scheduler::run() {
+	static const int max_events = 8*1024;
+	epoll_event events[max_events];
+	int rc, n_events;
+
+	for (;;) {
+		rc = epoll_wait(efd,&events[0],max_events,10);
+		if ( rc > 0 ) {
+			n_events = rc;
+			for ( int x=0; x<n_events; ++x ) {
+				Service& co = *(Service*)events[x].data.ptr;
+
+				co.ev_flags = events[x].events;
+				co.er_flags |= co.ev_flags & (EPOLLERR|EPOLLHUP|EPOLLRDHUP);
+
+				if ( !yield(co) )
+					delete &co;	// Coroutine is done
+				else if ( co.ev.sync_ev() )
+					chg(co.socket(),co.ev,&co);
+			}
+		} else if ( rc < 0 ) {
+			printf("Scheduler: %s: epoll_wait()\n",
+				strerror(errno));
+		}
+	}
+}
+
+// End scheduler.cpp
---
+++ scheduler.hpp
@@ -0,0 +1,57 @@
+//////////////////////////////////////////////////////////////////////
+// scheduler.hpp -- Epoll Cororoutine Class
+// Date: Sat Aug 11 14:48:30 2018   (C) Warren W. Gay ve3wwg@gmail.com
+///////////////////////////////////////////////////////////////////////
+
+#ifndef SCHEDULER_HPP
+#define SCHEDULER_HPP
+
+#include <stdint.h>
+#include <sys/epoll.h>
+#include <unordered_map>
+
+#include "coroutine.hpp"
+#include "events.hpp"
+#include "sockets.hpp"
+
+//////////////////////////////////////////////////////////////////////
+// Server processes the Service class:
+//////////////////////////////////////////////////////////////////////
+
+class Service : public Coroutine {
+	int		sock;			// Socket
+
+public:
+	Events		ev;
+	uint32_t	er_flags=0;		// Error flags
+	uint32_t	ev_flags=0;		// Event flags
+
+	Service(fun_t func,int fd) : Coroutine(func), sock(fd) {}
+	int socket() noexcept 			{ return sock; }
+};
+
+
+//////////////////////////////////////////////////////////////////////
+// epoll(2) Scheduler of Services
+//////////////////////////////////////////////////////////////////////
+
+class Scheduler : public CoroutineMain {
+	int		efd = -1;		// From epoll_create1()
+
+	std::unordered_map<int/*fd*/,CoroutineBase*> fdset;
+
+public:	Scheduler();
+	~Scheduler();
+	void close(int fd);
+	void run();
+
+	void sync(Events& ev) noexcept;
+
+	bool add(int fd,uint32_t events,Service *co);
+	bool del(int fd);
+	bool chg(int fd,Events& ev,CoroutineBase *co);
+};
+
+#endif // SCHEDULER_HPP
+
+// End scheduler.hpp
