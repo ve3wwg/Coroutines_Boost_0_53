dfe964c13b833493fc3376e501d5e89381f3f8aa
Warren Gay VE3WWG <ve3wwg@gmail.com>
2018-10-13 00:24:26 -0400

    Completed implementation

--- evtimer.hpp
+++ evtimer.hpp
@@ -7,8 +7,10 @@
 #define EVTIMER_HPP
 
 #include <time.h>
+#include <assert.h>
 #include <boost/intrusive/list.hpp>
 #include "circarray.hpp"
+#include "utility.hpp"
 
 typedef boost::intrusive::list_member_hook<> EvNode;	// Node to be included in Object
 
@@ -17,37 +19,67 @@ class EvTimer {
 	typedef boost::intrusive::member_hook<Object,EvNode,&Object::evnode> MemberHook;
 	typedef boost::intrusive::list<Object,MemberHook> ObjList;
 
-	time_t		epoch=0;			// carray[0] begins at this time (needs to be timespec)
+	timespec		epoch;			// carray[0] begins at this time (needs to be timespec)
+	timespec		incr;			// Time increment as timespec
+	long			incr_ms;		// Time increment in ms
 
-	struct s_tnode {
-		ObjList		list;
-	};
-
-	CircArray<s_tnode>	carray;
-
-public:	EvTimer(unsigned secs_max,unsigned granlarity_ms) noexcept;
-	void insert(unsigned x,Object& object) noexcept;
+	CircArray<ObjList>	carray;
 
 	void visit(unsigned x,void (*cb)(Object& object,void *arg),void *arg) noexcept;		// Needs to be private..
+
+public:	EvTimer(unsigned secs_max,unsigned granlarity_ms) noexcept;
+	EvTimer& insert(long ms,Object& object) noexcept;
+	EvTimer& expire(const timespec& now,void (*cb)(Object& object,void *arg),void *arg) noexcept;
 };
 
 
 template<typename Object>
-EvTimer<Object>::EvTimer(unsigned secs_max,unsigned granularity_ms) noexcept : carray(secs_max * 1000u / granularity_ms) {
-}
+EvTimer<Object>::EvTimer(unsigned secs_max,unsigned granularity_ms) noexcept : carray(secs_max * 1000u / granularity_ms + 1) {
 
+	incr.tv_sec = granularity_ms / 1000L;
+	incr.tv_nsec = granularity_ms * 1000000L;
+	incr_ms = incr.tv_sec * 1000L + incr.tv_nsec / 1000000L;
+	timeofday(epoch);
+}
 
 template<typename Object>
-void
-EvTimer<Object>::insert(unsigned x,Object& object) noexcept {
-	carray[x].list.push_back(object);
+EvTimer<Object>&
+EvTimer<Object>::insert(long time_ms,Object& object) noexcept {
+	timespec now;
+	long incr_time_ms, x;
+
+	timeofday(now);
+	now -= epoch;
+	incr_time_ms = millisecs(now) + time_ms;
+	x = incr_time_ms / incr_ms;
+
+	if ( x > long(carray.size()) )
+		x = long(carray.size()) - 1;
+	carray[x].push_back(object);
+	return *this;
 }
 
+template<typename Object>
+EvTimer<Object>&
+EvTimer<Object>::expire(const timespec& now,void (*cb)(Object& object,void *arg),void *arg) noexcept {
+
+	for ( ; epoch <= now; epoch += incr ) {
+		ObjList& list = carray[0];
+
+		while ( !list.empty() ) {
+			Object& object = list.front();
+			list.pop_front();
+			cb(object,arg);
+		}
+		carray.advance(1);
+	}
+	return *this;
+}
 
 template<typename Object>
 void
 EvTimer<Object>::visit(unsigned x,void (*cb)(Object& object,void *arg),void *arg) noexcept {
-	ObjList& list = carray[x].list;
+	ObjList& list = carray[x];
 	
 	while ( !list.empty() ) {
 		Object& object = list.front();
