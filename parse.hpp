312b9f20c356695765e4d6fd1c347ebb911fbf1a
Warren Gay <ve3wwg@gmail.com>
2018-10-27 11:46:21 -0400

    Improvements

--- parse.hpp
+++ parse.hpp
@@ -11,6 +11,8 @@
 #include <unordered_set>
 #include <vector>
 
+#include "utility.hpp"
+
 template<class C>
 void
 put(std::set<std::string>& container,const std::string& arg) {
@@ -37,34 +39,85 @@ put(std::vector<std::string>& container,const std::string& arg) {
 
 template<class C>	// Container
 void
-parse_fields(const char *source,size_t offset,C& container) {
-	size_t sz = strlen(source);
+parse_fields(C& container,const char *source) {
+	int n;
 	
-	auto OWS = [&]() {
-		if ( offset >= sz )
+	for (;;) {
+		source += strspn(source," \t;");
+		if ( !*source )
 			return;
-		offset += strspn(source+offset," \t");
-		if ( offset > sz )
-			offset = sz;
-	};
-	
-	auto field = [&]() -> std::string {
-		size_t start_offset = offset;
-		offset += strcspn(source + offset," \t;");
-		return std::move(std::string(source+start_offset,offset-start_offset));
-	};
 
-	do	{
-		OWS();		// Skip optional white space
-		const std::string word = field();
+		n = strcspn(source," \t;\r\n");
+		std::string word(source,n);
 
 		if ( !word.empty() )
 			put<C>(container,word);
 
-		OWS();
-		if ( offset < sz && source[offset] == ';' )
-			++offset;
-	} while ( offset < sz );
+		source += size_t(n);
+	}
+}
+
+//////////////////////////////////////////////////////////////////////
+// Parse source into a comma separated list of keyword=value pairs.
+//////////////////////////////////////////////////////////////////////
+
+template<class M>		// Map container
+void
+parse_tomap(M& map,const char *source) {
+	std::string segment;
+	char *v;
+	int n;
+
+	for (;;) {
+		n = strspn(source," ,\t\r\n");
+		source += n;
+		if ( !*source )
+			return;
+
+		n = strcspn(source," \t,\r\n");
+		if ( n <= 0 )
+			continue;		// Empty value
+
+		segment.assign(source,size_t(n));
+		v = (char *)strchr(segment.c_str(),'=');
+		if ( v ) {
+			*v++ = 0;		// NB: This modifies internal data of segment
+			map[segment] = v;	// Keyword = data pair
+		} else	{
+			map[segment];		// Just keyword
+		}
+		source += size_t(n);
+	}
+}
+
+//////////////////////////////////////////////////////////////////////
+// Parse fields separated by ';' into keyword maps
+//////////////////////////////////////////////////////////////////////
+
+template<class M>				// Map within vector
+void
+parse_fields(std::vector<M>& vector,const char *source) {
+	std::string segment;
+	int n;
+
+	for (;;) {
+		source += strspn(source," \t;\r\n");
+		n = strcspn(source,";\r\n");
+
+		if ( !*source )
+			return;
+		if ( n > 0 )
+			segment.assign(source,n);
+		else	segment = source;
+
+		vector.emplace_back();
+		M& map = vector.back();
+
+		parse_tomap(map,segment.c_str());
+		if ( map.empty() )
+			vector.erase(vector.end()-1);
+		source += size_t(n);
+	}
 }
 
 #endif // PARSE_HPP
